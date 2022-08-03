dcd82b0d4aed4b5504e89c4e23527c0196be3914
rashedul28 <rashedulislamridoy@gmail.com>
2022-08-03 23:50:24 +0600

    some route api added

--- backEnd/eLearning/routes/api.php
+++ backEnd/eLearning/routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\AdminAPIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/dashboard", [AdminAPIController::class, "dashboard"]);
+Route::get("/home", [AdminAPIController::class, "home"]);
+Route::get("/teacherAddRequest", [AdminAPIController::class, "teacherAddRequest"]);
+Route::get("/teacherList", [AdminAPIController::class, "teacherList"]);
+Route::get("/studentList", [AdminAPIController::class, "studentList"]);
+Route::get("/courseList", [AdminAPIController::class, "courseList"]);
